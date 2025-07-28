ac111aceb678eaafd85ea66fc2b117116132639c
Mustafa Özdemir <mustafa.ozedmir1408@gmail.com>
2025-07-28 13:00:55 +0200

    before Twig Partials & for Loops

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./assets/**/*.html",
+    "./assets/**/*.js",
+    "./assets/**/*.vue",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
